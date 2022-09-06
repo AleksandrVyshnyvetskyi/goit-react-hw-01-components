1b7ec3fd9f32befa1528c6aa2db084ca21955e1b
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-09-06 21:08:01 +0300

    strart project

---
+++ src/components/Profile/profile.js
@@ -0,0 +1,11 @@
+import user from '../Profile/user.json';
+
+<Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+/>
+
+console.log(Profile)
