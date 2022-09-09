a85131130813ee65d02d43923e593c51488b6c45
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-09-09 19:11:22 +0300

    bagfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,16 @@
-// ==============================================================================================
-import { Profile } from './profile/profile';
-import user from './profile/user.json';
-// ==============================================================================================
-import {Statistic} from './statistics/statistics';
-import data from './statistics/data.json';
-// ==============================================================================================
-import { FriendList } from './friends/friends';
-import friends from './friends/friends.json';
-// ==============================================================================================
-import transactions from './transaction/transactions.json'
-import {TransactionHistory} from "./transaction/transactionHistory"
-// ==============================================================================================
-
-const titleStyle = {
-    color: 'white',
-    marginBottom:20,
-    fontSize: 32,
-    marginTop: 40,
-}
-
-// ==============================================================================================
-
 export const App = () => {
   return (
     <div
       style={{
-          backgroundColor: '#425a7d',
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-          textAlign: 'center',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
       }}
     >
-      <div >
-        <h2 style={titleStyle}>Profile</h2>
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-      </div>
-
-      <div>
-        <h2 style={titleStyle}>Statistic</h2>
-        <Statistic title="Upload stats" stats={data} />
-      </div>
-
-      <div>
-        <h2 style={titleStyle}>Frinds</h2>
-        <FriendList friends={friends} />
-      </div>
-
-      <div>
-        <h2 style={titleStyle}>Transaction Hictory</h2>
-        < TransactionHistory transactions={transactions} />
-      </div>
-
+      React homework template
     </div>
   );
 };
